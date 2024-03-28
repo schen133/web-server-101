5654333492ab66c6eea6e07a80bd34af5baf4216
Sifeng Chen <sifengchen2002@gmail.com>
2024-03-27 22:49:55 -0400

    socket now defined and created, need to figure out how to listen to port. Using GNU's socket facility

--- entry.c
+++ entry.c
@@ -10,11 +10,17 @@
 // 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <strings.h>
+
+// some constant
+#define SERVER_PORT 2345
+
 
 // web server entry 
 // argc is the # of strings pointed to by argv, so basically # of character arrays argv is pointing to  
 // there are no string, so a string would be an array of Characters I believe
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
   // first of all, it needs to sit there and just in idle
   // so we need a program that just runs infinitely until I call it quit
   // 
@@ -24,8 +30,30 @@ int main(int argc, char *argv[]) {
   // TODO: Make program sit in a port, and then the while loop will be listening to ports
   // First thing to do here is, being able to successfully make a HTTP request to a port while
   // this server is running. -> then we can figure out how to parse the request and etc....
+  // 
   
   // the program keeps running and is listening for requests
+  // let's make a listener first. Using some header file library
+  // I am listening to my local machine for now ofcourse. So this program I am writing right now
+  // will notice any connection attempting to hit my local machine
+  //
+  // How we can achieve this is using a socket
+  // The socket will be identified by an IP address concatenated with port number
+  // The server waits for incoming client request to the port by listening to the same port.
+  // Once it hits, since we were listening, the request is received. Then we do protocol logic....
+  //
+  
+  int listenfd;
+  
+  if ( (listenfd = socket(AF_INET, SOCK_STREAM, 0)) > 0) {
+    printf("socket created successfully");
+  }
+  else {
+    printf("something went wrong...");
+  } 
+  fflush(stdout);
+  
+  // nice! socket created successfully, now make socket listen to a specific port
   
   while (1) {
     // TODO: Let's try to 
